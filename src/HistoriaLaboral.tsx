d8fe5a9f864516830073caeb67b08b5c43b0d59d
LuisR2398 <luisarmandoreyessalinas@gmail.com>
2024-04-29 14:09:04 -0600

    x

--- src/HistoriaLaboral.tsx
+++ src/HistoriaLaboral.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
-import "./HistoriaLaboral.css"
+import "./HistoriaLaboral.css";
 import FormContainer from "./FormContainer";
 import HistorialLaboralTable from "./HistoriaLaboralTable";
 
@@ -12,69 +12,97 @@ const HistorialLaboralForm = () => {
   });
 
   const [historialLaboraltodos, setHistorialLaboraltodos] = useState<[]>([]);
-
+  interface Empleado {
+    id: number;
+    nombres: string;
+    apellidos: string;
+  }
+  
+  const [dataEmpleado, setDataEmpleado] = useState<Empleado[]>([]);
   const fechaInicioRef = useRef<HTMLInputElement>(null);
-  const idEmpleadoRef = useRef<HTMLInputElement>(null);
-  const idPuestoRef = useRef<HTMLInputElement>(null);
+  const idEmpleadoRef = useRef<HTMLSelectElement>(null);
+  const idPuestoRef = useRef<HTMLSelectElement>(null);
 
   useEffect(() => {
     fetchData();
-  },[])
-  
+    fetchDataEmpleado();
+  }, []);
 
-  const handleSubmit = (e:any) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
-    if (fechaInicioRef.current && idEmpleadoRef.current && idPuestoRef.current) {
-        const newHistorialLaboral = {
-          fecha_inicio:  fechaInicioRef.current.value,
-          id_empleado: idEmpleadoRef.current.value,
-          id_de_puesto: idPuestoRef.current.value,
-        };
-        console.log(newHistorialLaboral)
-    axios
-      .post("http://localhost:4000/historialaboral", newHistorialLaboral)
-      .then((response) => {
-        alert("Historial laboral creado correctamente");
-      })
-      .catch((error) => {
-        console.error("Error al crear historial laboral:", error);
-        alert("Ha ocurrido un error al crear el historial laboral.");
-      });
+    if (
+      fechaInicioRef.current &&
+      idEmpleadoRef.current &&
+      idPuestoRef.current
+    ) {
+      const newHistorialLaboral = {
+        fecha_inicio: fechaInicioRef.current.value,
+        id_empleado: idEmpleadoRef.current.value,
+        id_de_puesto: idPuestoRef.current.value,
+      };
+      console.log(newHistorialLaboral);
+      axios
+        .post("http://localhost:4000/historialaboral", newHistorialLaboral)
+        .then((response) => {
+          alert("Historial laboral creado correctamente");
+        })
+        .catch((error) => {
+          console.error("Error al crear historial laboral:", error);
+          alert("Ha ocurrido un error al crear el historial laboral.");
+        });
+    }
   };
-}
 
-const onDelete = (e: any) => {
-    axios.delete(`http://localhost:4000/historialaboral/${e.data.id_de_aerolinea}`).then(x => alert("Se ha eliminado el historial laboral correctamente"));
-  }
+  const onDelete = (e: any) => {
+    axios
+      .delete(`http://localhost:4000/historialaboral/${e.data.id_de_aerolinea}`)
+      .then((x) => alert("Se ha eliminado el historial laboral correctamente"));
+  };
 
   const onUpdate = (e: any) => {
-    axios.put(`http://localhost:4000/historialaboral/${e.data.id_de_aerolinea}`, e.data).then(x => alert("Se ha guardado el historial laboral correctamente."));
+    axios
+      .put(
+        `http://localhost:4000/historialaboral/${e.data.id_de_aerolinea}`,
+        e.data
+      )
+      .then((x) => alert("Se ha guardado el historial laboral correctamente."));
   };
 
-const fetchData = () => {
+  const fetchDataEmpleado = () => {
     axios({
       method: "get",
-      url: "http://localhost:4000/historialaboral"
-    }).then((response) => {
-      setHistorialLaboraltodos(response.data);
-    }).catch((error) => {
-      console.error("Error fetching historial alboral data:", error);
-      alert("Ha ocurrido un error al obtener los historiales laborales.");
-    });
+      url: "http://localhost:4000/empleados",
+    })
+      .then((response) => {
+        setDataEmpleado(response.data as Empleado[]);
+      })
+      .catch((error) => {
+        console.error("Error fetching empleados data:", error);
+        alert("Ha ocurrido un error al obtener los empleados.");
+      });
+  };
+
+  const fetchData = () => {
+    axios({
+      method: "get",
+      url: "http://localhost:4000/historialaboral",
+    })
+      .then((response) => {
+        setHistorialLaboraltodos(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching historial alboral data:", error);
+        alert("Ha ocurrido un error al obtener los historiales laborales.");
+      });
   };
 
   return (
     <FormContainer onSubmit={handleSubmit}>
       <div className="form-group">
         <label htmlFor="fechaInicio">Fecha de Inicio:</label>
-        <input
-          type="date"
-          id="fechaInicio"
-          ref={fechaInicioRef}
-          required
-        />
+        <input type="date" id="fechaInicio" ref={fechaInicioRef} required />
       </div>
-      <div className="form-group">
+      {/* <div className="form-group">
         <label htmlFor="idEmpleado">ID de Empleado:</label>
         <input
           type="text"
@@ -82,18 +110,28 @@ const fetchData = () => {
           ref={idEmpleadoRef}
           required
         />
-      </div>
+      </div> */}
       <div className="form-group">
-        <label htmlFor="idPuesto">ID de Puesto:</label>
-        <input
-          type="text"
-          id="idPuesto"
-          ref={idPuestoRef}
-          required
-        />
+        <label htmlFor="idEmpleado">ID de Empleado:</label>
+        <select name="idEmpleado" id="idEmpleado" ref={idEmpleadoRef} required>
+          {dataEmpleado.map((empleado) => (
+            <option key={empleado.id} value={empleado.id}>
+              {empleado.nombres} {empleado.apellidos}
+            </option>
+          ))}
+        </select>
       </div>
-      <button type="submit">Enviar</button>
-      <HistorialLaboralTable data={historialLaboraltodos} onDelete={onDelete} onEdit={onUpdate}/>
+
+      {/* <div className="form-group">
+        <label htmlFor="idPuesto">ID de Puesto:</label>
+        <input type="text" id="idPuesto" ref={idPuestoRef} required />
+      </div> */}
+      <button type="submit" className="submit-button">Enviar</button>
+      <HistorialLaboralTable
+        data={historialLaboraltodos}
+        onDelete={onDelete}
+        onEdit={onUpdate}
+      />
     </FormContainer>
   );
 };
